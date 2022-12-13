ef7122b674219d74d8058b07d2fa958c97413ea7
kingsaboor <120422145+kingsaboor@users.noreply.github.com>
2022-12-13 12:13:52 +0430

    kabul
    git clone https://github.com/kingsaboor/kabul.git
    
    ls
    
    cd kabul
    
    python kabul.py

---
+++ Kabul.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+#code=_-udf:8-_
+#creater : Mirwais Danishyar 
+
+import os,sys
+import time
+#from gtts import gTTS
+import string,random 
+
+#Main_Menu:-
+def Main():
+    os.system('clear')
+    print('\033[1;96m              PLEASE WAIT FOR UPDATE\n\033[1;92m              TEAM : AWM TEAM PRO\n------------------------------------------------')
+    print('\033[1;91m              RESPECT BRO....\n------------------------------------------------')
+    print('\033[1;93m              Mirwais Danishyar\n------------------------------------------------')
+    
+Main()
